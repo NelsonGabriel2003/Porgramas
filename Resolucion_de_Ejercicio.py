9169435108e48b41c2c6d5af2ec5122eba96b7d3
Alejandro Barrionuevo <Apbarrionuevo@puce.edu.ec>
2023-12-09 23:33:07 -0500

    Ejercicio #2 resuelto

--- Resolucion_de_Ejercicio.py
+++ Resolucion_de_Ejercicio.py
@@ -1,1 +1,44 @@
-print("Hola mundo")
+unidad1 = input("Ingresar unidad actual  mm, cm, m, hm, km: ")
+unidad2 = input("Ingresar unidad requerida mm, cm, m, hm, km: ")
+print("Conversion")
+valor = float(input("Ingresa valor: "))
+
+
+
+if unidad1 == "mm":
+    cantidad_en_metros = valor / 1000
+elif unidad1 == "cm":
+    cantidad_en_metros = valor / 100
+if unidad1 == "m":
+    cantidad_en_metros = valor
+elif unidad1 == "hm":
+    cantidad_en_metros = valor * 100
+elif unidad1 == "km":
+    cantidad_en_metros = valor * 1000
+else:
+    print("Unidad actual no reconocida.")
+
+
+    
+if unidad2 == "mm":
+    cantidad_convertida = cantidad_en_metros * 1000
+if unidad2 == "cm":
+    cantidad_convertida = cantidad_en_metros * 100
+if unidad2 == "m":
+    cantidad_convertida = cantidad_en_metros
+elif unidad2 == "hm":
+    cantidad_convertida = cantidad_en_metros / 100
+elif unidad2 == "km":
+    cantidad_convertida = cantidad_en_metros / 1000
+else:
+    print("Unidad requerida no reconocida.")
+        
+
+    
+print(f"{valor} {unidad1} son {cantidad_convertida} {unidad1}")
+
+
+
+
+
+
